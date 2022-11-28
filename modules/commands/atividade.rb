f01e63673d1ae3c8bd1fa07ee27572e8823591ce
Spyei <caio.salamao1234@gmail.com>
2022-11-28 15:20:55 -0300

    detalhes fix

--- modules/commands/atividade.rb
+++ modules/commands/atividade.rb
@@ -6,10 +6,14 @@ module Bot::DiscordCommands
                 user = event.user
                 user = event.message.mentions.first.on(event.server) unless event.message.mentions.empty?
                 s = user.activities.to_a.first
+                detalhes = s.details
                 if s == nil
                     event.respond "**( ❌ ) O usuário ou você está sem atividade no momento**"
+                elsif detalhes == nil
+                    detalhes = "sem detalhes"
+                    event.respond "> **Nome:** \`#{s.name}\`\n> **Detalhes:** \`#{detalhes}\`\n> **Data de inicio:** <t:#{s.timestamps.start.to_i}:R>"
                 else
-                    event.respond "> **Nome:** \`#{s.name}\`\n> **Detalhes:** \`#{s.details}\`\n> **Data de inicio:** <t:#{s.timestamps.start.to_i}:R>\`"
+                    event.respond "> **Nome:** \`#{s.name}\`\n> **Detalhes:** \`#{detalhes}\`\n> **Data de inicio:** <t:#{s.timestamps.start.to_i}:R>"
                 end
             rescue => error
                 event.respond("**( ❌ ) Erro econtrado: \`\`\`ruby\n#{error}\n\`\`\`**")
