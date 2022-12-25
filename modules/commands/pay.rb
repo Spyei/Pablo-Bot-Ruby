2c8f02b6c085b8149e76124a55dce51d4682c3bf
Spyei <caio.salamao1234@gmail.com>
2022-12-25 12:06:06 -0300

    adição comando de pay

--- modules/commands/pay.rb
+++ modules/commands/pay.rb
@@ -3,10 +3,23 @@ module Bot::DiscordCommands
       extend Discordrb::Commands::CommandContainer
       command [:pay, :pagar] do |event, *txt|
         user = event.user
-        user = event.message.mentions.first.on(event.server) unless event.message.mentions.empty?
-        event.respond "você pagou #{txt.join(" ").gsub("<@#{user.id}>", "").gsub(" ", "")} para <@#{user.id}>,"
-      rescue StandardError => error
-        event.respond("**( ❌ ) Erro econtrado: \`\`\`ruby\n#{error}\n\`\`\`**")
+        user = event.message.mentions.first.on(event.server)
+        quantia = txt.join(" ").gsub("<@#{user.id}>", "").gsub(" ", "")
+        if user == "" 
+          event.respond "**( ❌ )** Utilize da seguinte forma: \`p!pay <usuário> <quantia>\`."
+        elsif quantia == ""
+          event.respond "**( ❌ )** Digite uma quantia."
+        elsif quantia != quantia.to_i.to_s
+          event.respond "**( ❌ )** Digite uma quantia válida."
+        elsif user == event.author.id
+          event.respond "**( ❌ )** Você não pode se pagar."  
+        elsif user.bot_account == true
+          event.respond "**( ❌ )** Você não pode pagar bots."
+        else
+          event.respond "você pagou #{quantia} para <@#{user.id}>,"
+        end
+      rescue StandardError => penis_hetero
+        event.respond "**( ❌ )** Utilize da seguinte forma: \`p!pay <usuário> <quantia>\`"
     end
   end
 end
